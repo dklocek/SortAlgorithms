9725266b58db849f6beb7ccb7b3cd6fd031c6b30
Dawid Klocek <d.klocek@wp.pl>
2018-05-26 21:44:42 +0200

    Modified RestController

--- src/main/java/pl/dklocek/sorters/spring/SortController.java
+++ src/main/java/pl/dklocek/sorters/spring/SortController.java
@@ -1,14 +1,12 @@
 package pl.dklocek.sorters.spring;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import pl.dklocek.sorters.backend.BubbleSort;
-import pl.dklocek.sorters.backend.InsertionSort;
-import pl.dklocek.sorters.backend.SortMethod;
-import pl.dklocek.sorters.backend.StringToIntArray;
+import pl.dklocek.sorters.backend.*;
 
-
+@CrossOrigin
 @RestController
 public class SortController {
 
@@ -31,5 +29,25 @@ public class SortController {
 
         return sortMethod.sort(StringToIntArray.StringToIntArray(numbersString));
     }
+
+    @RequestMapping(value = "/quick")
+    public int[] quick(@RequestParam("table") String numbersString) {
+
+        if(numbersString.length()==0)return new int[0];
+        sortMethod = new QuickSort();
+
+        return sortMethod.sort(StringToIntArray.StringToIntArray(numbersString));
+    }
+
+    @RequestMapping(value = "/selection")
+    public int[] selection(@RequestParam("table") String numbersString) {
+
+        if(numbersString.length()==0)return new int[0];
+        sortMethod = new SelectionSort();
+
+        return sortMethod.sort(StringToIntArray.StringToIntArray(numbersString));
+    }
+
+
 
 }
--- src/main/java/pl/dklocek/sorters/spring/SortersApplication.java
+++ src/main/java/pl/dklocek/sorters/spring/SortersApplication.java
@@ -2,12 +2,26 @@ package pl.dklocek.sorters.spring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 
-public class SortersApplication {
+public class SortersApplication extends SpringBootServletInitializer{
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(SortersApplication.class, args);
 	}
+
+
+	/*
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application){
+		return application.sources(SortersApplication.class);
+	}
+
+	public static void main(String[] args) throws Exception{
+		SpringApplication.run(SortersApplication.class,args);
+	}
+      */
 }
